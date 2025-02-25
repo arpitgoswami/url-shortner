29d4fa1c452d59764f13627e8a5f95a8ce352b1f
arpitgoswami <arpitgoswami@hotmail.com>
2025-02-25 23:03:32 +0530

    UI Major Update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,30 @@
 import { FormEvent, useState } from "react";
 import axios from "axios";
-import ReCAPTCHA from "react-google-recaptcha";
-
-const SITE_KEY = "6LcKN-AqAAAAAAtVON7EVGjv-oz1yKcYf6QuGLQJ";
+import "./index.css";
+import toast, { Toaster } from "react-hot-toast";
 
 function App() {
-  const [url, setUrl] = useState<string>("");
-  const [captchaValue, setCaptchaValue] = useState<string | null>(null);
+  const [url, setUrl] = useState<string>(
+    "https://gradienty.codes/animated-icons"
+  );
   const [data, setData] = useState<{ id: string } | null>(null);
-  const [copySuccess, setCopySuccess] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
+
+  const [url_base, setUrlBase] = useState<string>(
+    " https://stake-mines-1.onrender.com"
+  );
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setLoading(true);
 
-    if (!captchaValue) {
-      alert("Please solve the CAPTCHA before submitting.");
-      return;
-    }
-
     try {
-      const response = await axios.post(
-        "https://stake-mines-1.onrender.com/api/short",
-        { website: url, captcha: captchaValue }
-      );
+      const response = await axios.post(`${url_base}/api/short`, {
+        website: url,
+      });
 
       setData(response.data);
       setLoading(false);
-      setCopySuccess(""); // Reset copy message
     } catch (error) {
       console.error("Error:", error);
       alert("Something went wrong! Please try again.");
@@ -37,52 +33,80 @@ function App() {
 
   const handleCopy = () => {
     if (data) {
-      const shortUrl = `https://stake-mines-1.onrender.com/${data.id}`;
+      const shortUrl = `${url_base}/${data.id}`;
       navigator.clipboard.writeText(shortUrl);
-      setCopySuccess("Copied!");
-      setTimeout(() => setCopySuccess(""), 2000); // Reset message after 2s
+      toast.success("Copied to clipboard!");
     }
   };
 
   return (
-    <div>
-      <h1>Urlify</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={url}
-          onChange={(e) => setUrl(e.target.value)}
-          placeholder="Enter URL..."
-          required
-        />
-        <ReCAPTCHA
-          sitekey={SITE_KEY}
-          onChange={(value) => {
-            console.log("CAPTCHA Value:", value); // Debugging
-            setCaptchaValue(value);
-          }}
-        />
-        <button type="submit" disabled={loading}>
-          {loading ? "Loading .." : "Submit"}
-        </button>
-      </form>
+    <div className="container-gradient p-12 w-[100vw] h-[100vh] text-white">
+      <div>
+        <h1 className="text-gray-500">
+          URL SHORTNER
+          <span className="bg-blue-500 ml-2 text-white text-sm rounded px-2">
+            BETA
+          </span>
+        </h1>
 
-      {data && (
-        <div>
-          <p>
-            Shortened URL:{" "}
-            <a
-              href={`https://stake-mines-1.onrender.com/${data.id}`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              https://stake-mines-1.onrender.com/{data.id}
-            </a>
-          </p>
-          <button onClick={handleCopy}>Copy URL</button>
-          {copySuccess && <span> {copySuccess}</span>}
-        </div>
-      )}
+        <p className="my-12 text-4xl font-medium">
+          Shorten your long URLs into brief, shareable links.
+        </p>
+      </div>
+
+      <div className="mb-12">
+        {data.id && (
+          <div className="text-gray-500 bg-black/50 hover:-translate-y-1 duration-200 p-8 rounded-lg space-y-3 text-sm max-w-2xl">
+            <div className="flex justify-between">
+              <div className="flex items-center">
+                <div className="mr-3 h-2 w-2 rounded-full bg-green-400"></div>
+                <p className="font-medium text-blue-300">
+                  <a
+                    href={`${url_base}/${data.id}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {url_base}/{data.id}
+                  </a>
+                </p>
+              </div>
+
+              <div>Created Link</div>
+            </div>
+
+            <div className="text-white text-lg">{url}</div>
+
+            <div className="flex justify-between items-center">
+              <div>Never Expires</div>
+              <button
+                onClick={handleCopy}
+                className="bg-blue-500 text-white cursor-pointer p-1 text-sm rounded-lg hover:bg-blue-600 duration-200"
+              >
+                Copy URL
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+
+      <div>
+        <form
+          onSubmit={handleSubmit}
+          className="bg-[#0a0d14] gap-4 max-w-lg box flex justify-between py-5 px-10 element rounded-lg"
+        >
+          <input
+            type="url"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+            placeholder="Enter URL..."
+            className="w-full"
+            required
+          />
+          <button type="submit" disabled={loading}>
+            {loading ? "Loading" : "Submit"}
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
